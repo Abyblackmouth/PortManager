a0aa4a25059b3a5b72a6254d9b529d9bd0579e51
abyblackmouth <abraham.covarrubias@live.com>
2023-04-24 22:58:48 -0600

    add coonection bd moongose

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const mongoose = require("mongoose")
+
+const DB_USER = 'Abraham'
+const DB_PASSWORD = 'Abraham32'
+const DB_NAME = 'PortManager'
+const DB_HOST = 'atlascluster.qpaiavk.mongodb.net'
+
+const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+
+mongoose.connect(URL)
+    .then((connection)=>{
+        console.log('Database conected :D')
+    })
+    .catch((error)=>{
+        console.log('Error', error)
+    })
+
 
 const { setServers } = require("dns")
 const http = require("http")
