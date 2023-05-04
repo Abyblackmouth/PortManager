68b0b0fdae3fed8f1f0f83ada14344e442c18a89
abyblackmouth <abraham.covarrubias@live.com>
2023-05-04 08:43:23 -0600

    add bd clientes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 require('dotenv').config()
+const clientesSchema = require("./models/clientes.model")
 
 const {DB_USER, DB_PASSWORD, DB_NAME, DB_HOST} = process.env
 const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
@@ -12,14 +13,19 @@ const server = http.createServer((request, response)=>{
     response.end()
 })
 
+
+const clientes = mongoose.model('clientes', clientesSchema)
+
 mongoose.connect(URL)
-    .then((connection)=>{
+    .then( async (connection)=>{
         console.log('Database conected :D')
+
+        const allClientes = await clientes.find({})
+        console.log(allClientes)
 
         server.listen(8080,()=>{
             console.log("Servidor Conectado")
-        })
-        
+        })  
     })
     .catch((error)=>{
         console.log('Error', error)
